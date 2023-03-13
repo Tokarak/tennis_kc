39a5f94cb5960754d053e270a15d4dc9dc8ae824
Tokarak <63452145+Tokarak@users.noreply.github.com>
2023-03-13 17:47:46 +0000

    Fix decoding error

--- srver_points_rally_duration.py
+++ srver_points_rally_duration.py
@@ -4,9 +4,9 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 df_m = pd.read_csv(
-    "tennis_MatchChartingProject/charting-m-points-from-2017.csv")
+    "tennis_MatchChartingProject/charting-m-points-from-2017.csv", encoding="ISO-8859-1")
 df_w = pd.read_csv(
-    "tennis_MatchChartingProject/charting-w-points-from-2017.csv")
+    "tennis_MatchChartingProject/charting-w-points-from-2017.csv", encoding="ISO-8859-1")
 
 for (df, label) in [(df_m, "M"), (df_w, "F")]:
     # Check who wins the point
