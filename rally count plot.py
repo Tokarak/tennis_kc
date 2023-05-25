01c02e6f6fd0e12acd9d3662f8378cd21bb36096
Tokarak <63452145+Tokarak@users.noreply.github.com>
2023-05-25 14:07:43 +0100

    Reformat old script

--- rally count plot.py
+++ rally count plot.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 df_m = pd.read_csv(
     "tennis_MatchChartingProject/charting-m-points-from-2017.csv", encoding="ISO-8859-1")
-df=df_m[["rallyCount", "PtWinner"]]
+df = df_m[["rallyCount", "PtWinner"]]
 # df=df_m[df_m["isForced"]==0][["rallyCount", "PtWinner"]]
-rallyct=df.groupby("rallyCount").count().astype(int, errors="ignore")
-t=rallyct.values[:,0]
+rallyct = df.groupby("rallyCount").count().astype(int, errors="ignore")
+t = rallyct.values[:, 0]
 plt.plot(t)
 plt.show()
-X=[]
+X = []
 for (i, v) in enumerate(zip(t, t[1:])):
-    X.append(v[1]/v[0])
-X=np.array(X)
+    X.append(v[1] / v[0])
+X = np.array(X)
 plt.plot(X[4:30])
 plt.show()
